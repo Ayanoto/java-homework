42afd41beb22a460fc3eb6423c2833caf45e805c
Ayanoto <91425576+Ayanoto@users.noreply.github.com>
2021-11-12 18:20:32 +0800

    Add files via upload

---
+++ 第六次作业/demo/src/main/java/com/example/Location.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public class Location {
+    public int row, column;
+    public double maxValue;
+}
---
+++ 第六次作业/demo/src/main/java/com/example/code9_3.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.swing.text.html.StyleSheet;
+
+//import java.util.Date;
+public class code9_3 {
+    public static void main(String[] args) {
+        java.util.Date date = new java.util.Date();
+        long time = 10000;
+        for (int i = 1; i <= 8; i++) {
+            date.setTime(time);
+            System.out.println(date.toString());
+            time *= 10;
+        }
+    }
+}
